a15953d622c2792ee05d69cac8633f3b1b55567d
Krikatiny <damerdaner21@gmail.com>
2023-07-05 04:27:24 +0300

    Completed Task3 with file Task3.c

---
+++ Task3/Task3.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "math.h"
+#include "stdlib.h"
+
+int main(){
+    //Getting p
+    int sum = 0, num, result = 0;
+    printf("Enter p: ");
+    scanf("%d", &num);
+
+    //2^p
+    result = pow(2, num);
+
+    //sum = i^1 + i^2 + i^3 ... i^num-2
+    for (int i = 0; i < num-2; i++) {
+        sum = pow(2, i+1) + sum;
+    }
+
+    //2^p - sum
+    result = result - sum;
+
+    printf("%d\n", result);
+    system("pause");
+    return 0;
+}
