c36897378800eecf8d1be47b61eb96b20702ef27
Krikatiny <damerdaner21@gmail.com>
2023-07-05 04:31:29 +0300

    Added p(<=30) check

--- Task3/Task3.c
+++ Task3/Task3.c
@@ -5,21 +5,27 @@
 int main(){
     //Getting p
     int sum = 0, num, result = 0;
-    printf("Enter p: ");
+    printf("Enter p (p <= 30): ");
     scanf("%d", &num);
 
-    //2^p
-    result = pow(2, num);
+    if(num >= 30) {
+        //2^p
+        result = pow(2, num);
 
-    //sum = i^1 + i^2 + i^3 ... i^num-2
-    for (int i = 0; i < num-2; i++) {
-        sum = pow(2, i+1) + sum;
+        //sum = i^1 + i^2 + i^3 ... i^num-2
+        for (int i = 0; i < num - 2; i++) {
+            sum = pow(2, i + 1) + sum;
+        }
+
+        //2^p - sum
+        result = result - sum;
+
+        printf("%d\n", result);
+        system("pause");
+        return 0;
+    } else{
+        printf("Error!");
+        system("pause");
+        return 1;
     }
-
-    //2^p - sum
-    result = result - sum;
-
-    printf("%d\n", result);
-    system("pause");
-    return 0;
 }
