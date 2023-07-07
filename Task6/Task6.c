00725ba069f0ffcf600b3653246fcaa0b22ce88b
Krikatiny <damerdaner21@gmail.com>
2023-07-07 19:03:54 +0300

    Upload Task6 comments

--- Task6/Task6.c
+++ Task6/Task6.c
@@ -3,13 +3,16 @@
 #include "math.h"
 
 int main(){
+    //Declaring and getting parameters
     int x1, y1, x2, y2;
     double result;
     printf("x1, y1, x2, y2:\n");
     scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
 
-
+    //Calculating
     result = sqrt((pow((x2-x1),2) + pow(y2-y1,2)));
+
+    //Output
     printf("Result: %.6f\n", result);
     system("pause");
     return 0;
