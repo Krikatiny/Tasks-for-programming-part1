dca7120093ac60b25448b0533beca6686530b021
Krikatiny <damerdaner21@gmail.com>
2023-07-07 19:34:34 +0300

    Upload Task7

--- Task7/Task7.c
+++ Task7/Task7.c
@@ -3,10 +3,15 @@
 #include "math.h"
 
 int main(){
+    //Declaring and input parameters
     int num, x1, y1, r1, x2, y2, r2;
     printf("Enter x1, y1, r1, x2, y2, r2:\n");
     scanf(" %d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
 
+    //x1 == x2 and y1 == y2 and r1 == r2: num = -1
+    //|o1o2| > r1 + r2 : num = 0
+    //|o1o2| = r1 + r2 : num = 1
+    //|o1o2| < r1 + r2 : num = 2
     if(x1 == x2 && y1 == y2 && r1 == r2){
         num = -1;
     } else{
@@ -21,7 +26,7 @@ int main(){
         }
     }
 
-
+    // Output
     printf("Num of points: %d\n", num);
     system("pause");
     return 0;
