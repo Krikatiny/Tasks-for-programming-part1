40c9f4d8d957ed75de3e006d58b0660365b788d1
Krikatiny <damerdaner21@gmail.com>
2023-07-07 19:29:09 +0300

    Upload Task7

---
+++ Task7/Task7.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+int main(){
+    int num, x1, y1, r1, x2, y2, r2;
+    printf("Enter x1, y1, r1, x2, y2, r2:\n");
+    scanf(" %d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
+
+    if(x1 == x2 && y1 == y2 && r1 == r2){
+        num = -1;
+    } else{
+        if (sqrt((pow((x2-x1),2) + pow(y2-y1,2))) > r1 + r2){
+            num = 0;
+        }
+        if (sqrt((pow((x2-x1),2) + pow(y2-y1,2))) == r1 + r2){
+            num = 1;
+        }
+        if (sqrt((pow((x2-x1),2) + pow(y2-y1,2))) < r1 + r2){
+            num = 2;
+        }
+    }
+
+
+    printf("Num of points: %d\n", num);
+    system("pause");
+    return 0;
+}
